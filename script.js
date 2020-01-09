70d2cbc6910e6a4591324974646d06a3b766daf0
aagrosse <aagrock81@yahoo.com>
2020-01-09 05:18:18 -0500

    added in pseudocode in script for clarity

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+//set moment to a variable
 var now = moment();
+//create a variable for the current date
 var currentDate = now.format("MMM Do YYYY");
 
 // Set date in Jumbotron
@@ -8,7 +10,7 @@ $("#currentDay").text("Today's Date: " + currentDate);
 
 $(document).ready(function () {
 
-// For loop to get and print tasks from local storage
+// For loop to get and display tasks from local storage
     hourArr = $('.hour').toArray()
     for (i = 0; i < hourArr.length; i++) {
 
@@ -19,24 +21,35 @@ $(document).ready(function () {
 
 // For loop to print rows with timeblocks, taskblocks, and save buttons
 for (i = 0; i < 9; i++) {
+    //create a variable for the row
     var rowBlock = $('<div>').addClass('row')
+    //create a variable for the time block
     var timeBlock = $('<div>').addClass('hour col-md-2').text(moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'))
     timeBlock.attr('data-time', moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'))
+    //create a variable for the taskblock
     var taskBlock = $('<textarea>').addClass('col-md-9')
+    //create a variable for the save block
     var saveButton = $('<button>').addClass('saveBtn col-md-1').html('<i class="fas fa-save"></i>')
 
-    // Placing content created above into the proper elements
+// Placing content created above into the DOM in the right order.
+    //append the container with the row
     $('.container').append(rowBlock)
+    //append the row with the time block
     $(rowBlock).append(timeBlock)
+    //after the timeblock display the task block
     $(timeBlock).after(taskBlock)
+    //after the taskblock display the save button
     $(taskBlock).after(saveButton)
 
 
 // if else statement to determine the color of the row
+    //if the time is the same as the time on the timeblock, display task block as red
     if (now.isSame(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
         $(taskBlock).addClass('present')
+    //if the time is in the future, display green
     } else if (now.isBefore(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
         $(taskBlock).addClass('future')
+    //if the time is in the past, display grey
     } else if (now.isAfter(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
         $(taskBlock).addClass('past')
     }
@@ -48,5 +61,5 @@ $('.saveBtn').on('click', function () {
     localStorage.setItem($(this).siblings('div.hour').attr('data-time'), $(this).siblings('textarea').val())
 })
 
-console.log('data-time', moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'))
+
 
